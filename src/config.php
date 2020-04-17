e4a438ccf8923baa294f6eaeef90f29d64bb5cce
shyam-biradar <shyam.biradar@trilio.io>
2020-04-17 05:53:20 -0400

    Added code to write and read data

---
+++ src/config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+  * Configuration for database connection
+  *
+  */
+
+$host       = "k8s-demo-app-mysql";
+$username   = "root";
+$password   = "dHJpbGlvcGFzcwo=";
+$dbname     = "test"; // will use later
+$dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
